3f0458ce548b03bf63ed966fb3bb9ff8e551c495
iamnijin <35027680+iamnijin@users.noreply.github.com>
2020-07-07 13:35:22 +0530

    Add files via upload

---
+++ LINUX/MAIN.py
@@ -0,0 +1,7 @@
+class MAIN:
+    def website_list():
+        website_list=["www.facebook.com","www.youtube.com"] #ADD THE WEBSITES HERE TO BLOCK
+        return(website_list)
+    def hosts_path():
+         hosts_path = r"/etc/hosts"    # r is for raw string
+         return (hosts_path)
